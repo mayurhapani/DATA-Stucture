7dd152a4c72cfd8c994a1f23a3a38212882e2008
mayurhapani <hapanimayur@gmail.com>
2024-01-17 14:33:32 +0530

    demo comment

--- home demos/margeshort.js
+++ home demos/margeshort.js
@@ -3,6 +3,7 @@ let arr = [8, 10, 2, 9, 1, 6, 0, 7];
 function margeSort(arr, si, ei) {
   let mid = (si + ei) / 2;
   console.log(mid);
+  console.log(si, ei);
 }
 
 margeSort(arr, 0, arr.length - 1);
